5fe78d7019d2814278c380cb36fa9c1e341e45b4
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2018-01-31 12:25:45 +0100

    Revert "Mass truncate layer misleading 500 response when nothing is cached #587"
    This reverts commit 69d182a60b9f00be506a701ba373192ae0020d25.

--- geowebcache/core/src/main/java/org/geowebcache/seed/TruncateLayerRequest.java
+++ geowebcache/core/src/main/java/org/geowebcache/seed/TruncateLayerRequest.java
@@ -6,8 +6,6 @@ import org.geowebcache.storage.StorageException;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-import java.util.stream.Stream;
-
 /**
  * A request to completely truncate a layer's cache.
  * 
@@ -19,14 +17,7 @@ public class TruncateLayerRequest implements MassTruncateRequest {
     String layerName;
 
     public boolean doTruncate(StorageBroker sb, Configuration config) throws StorageException {
-        boolean truncated = sb.delete(layerName);
-        if (!truncated) {
-            // did we hit a layer that has nothing on storage, or a layer that is not there?
-            if(config.getTileLayer(layerName) == null) {
-                throw new IllegalArgumentException("Could not find layer " + layerName);
-            }
-        }
-        return true;
+        return sb.delete(layerName);
     }
 
 }
